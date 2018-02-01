b15bcbc5ce44bb471100d1187f7a73d3187afea0
muschellij2 <muschellij2@gmail.com>
2018-02-01 11:07:14 -0500

    updated description with doi

--- R/get_smri_filenames.R
+++ R/get_smri_filenames.R
@@ -3,7 +3,7 @@
 #' @description Return the filenames for the Structural images
 #' @param ... arguments to pass to \code{\link{get_image_filenames}},
 #' \code{modalities = c("T1", "T2", "FLAIR"} so it cannot be specified
-#' @return Vector fo filenames
+#' @return Vector of filenames
 #' 
 #' @examples
 #' get_smri_filenames()
